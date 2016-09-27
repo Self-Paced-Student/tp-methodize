89c29f01c046ddd450ce7f3d5de499aa47ab56a2
Alessandro Zanardi <alessandro@zanardi.org>
2016-09-27 04:34:39 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+// Methodize (20 mins)
+
+// A binary function is a function that takes two inputs
+// and returns a value, like for example add(3,5) -> 8.
+// You need to write a function named "methodize", that
+// converts a binary function to a method. For example:
+//
+// function add (a, b) {return a + b;};
+// Number.prototype.add = methodize(add);
+// (3).add(5) -> 8
+
+function methodize (func) {
+
+}
+
+module.exports = methodize;
